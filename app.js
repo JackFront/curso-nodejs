5e4610320b56b7be4ef064952d50ff0b61ca72c4
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-11-19 15:45:05 -0300

    Adicionado as views do projeto inicial

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs')
+
+app.get('/tecnologia', (req, res) => {
+    res.render("secao/tecnologia")
+})
+
 app.get('/', (req, res) => {
     res.send("<html><body>Portal de notícias</body></html>")
 })
 
-app.get('/tecnologia', (req, res) => {
-    res.send("<html><body>Noticias de tecnologia</body></html>")
-})
+
 
 app.listen(3000, () => {
     console.log("Servidor rodando com Express")
