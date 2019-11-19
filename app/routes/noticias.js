136d3884856e885add4b9f89dc937f4efa0fd09a
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-11-19 17:42:36 -0300

    colocando a conexão com a base de dados em um novo módulo

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,15 +1,9 @@
+var connection = require('../../config/dbConnection')();
 module.exports = (app) => {
 
     app.get('/noticias', (req, res) => {
 
-        var mysql = require('mysql');
 
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'root',
-            database: 'portal_noticias'
-        })
 
         connection.query('SELECT * FROM noticias', (error, result) => {
             res.render("noticias/noticias", {
