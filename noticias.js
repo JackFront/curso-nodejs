e74fab8eefe0da23fce1a97abf468b76263f4ad3
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-11-19 15:23:02 -0300

    Seção 1 Node rota e response http

---
+++ noticias.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+    var categoria = req.url
+
+    if (categoria == '/tecnologia') {
+        res.end("<html><body>Noticias de tecnologia</body></html>")
+
+    } else if (categoria == '/moda') {
+        res.end("<html><body>Noticias de moda</body></html>")
+
+    } else if (categoria == '/beleza') {
+        res.end("<html><body>Noticias de beleza</body></html>")
+    } else {
+        res.end("<html><body>Portal de notícias</body></html>")
+    }
+
+}).listen(3000);
