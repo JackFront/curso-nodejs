4838fcb6fc357a332452ad303214a0106ef1afe9
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-11-19 22:50:56 -0300

    Recebendo dados do formulário com body-parser

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,4 +2,8 @@ module.exports = (app) => {
     app.get('/form_inclusao_noticia', (req, res) => {
         res.render("admin/form_add_noticia")
     })
+    app.post('/noticias/salvar', (req, res) => {
+        var noticias = req.body;
+        res.send(noticias);
+    })
 }
