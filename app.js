faa70cbe14a829045a345a7be7e7cc69c1c09d90
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-11-19 22:01:25 -0300

    Refactoring das rotas

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('./config/server')
-const rotaNoticias = require('./app/routes/noticias')(app);
-const rotaHome = require('./app/routes/home')(app);
-const rotaForm = require('./app/routes/formulario_inclusa_noticia')(app);
+// const rotaNoticias = require('./app/routes/noticias')(app);
+// const rotaHome = require('./app/routes/home')(app);
+// const rotaForm = require('./app/routes/formulario_inclusa_noticia')(app);
 
 
 app.listen(3000, () => {
